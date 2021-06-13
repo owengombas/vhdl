2c587e3cdc46e6db4b6f727b04c2cff289a0ca63
clement.briglian <clement.brigliano@he-arc.ch>
2021-06-14 00:05:40 +0200

    finished alu and muliplexer

--- src/sysnumapp_microcontroleur.srcs/sources_1/new/ALU.vhd
+++ src/sysnumapp_microcontroleur.srcs/sources_1/new/ALU.vhd
@@ -21,6 +21,8 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use Work.nanoProcesseur_package.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -33,17 +35,211 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity ALU is
     Port(
-        Oper1_i: in  std_logic_vector(7 downto 0);
-        Oper2_i: in  std_logic_vector(7 downto 0);
-        CCR_i: in  std_logic_vector(3 downto 0);
-        Opcode_i: in  std_logic;
-        ALU_result_o: in  std_logic_vector(8 downto 0)
-    );
+         Oper1_i : in  std_logic_vector(7 downto 0);
+         Oper2_i : in  std_logic_vector(7 downto 0);
+         Opcode_i    : in  std_logic_vector(5 downto 0);
+         CCR_i       : in  std_logic_vector(3 downto 0);
+         CCR_alu_o   : out std_logic_vector(3 downto 0);
+         Accu_o   : out std_logic_vector(7 downto 0)
+        );
 end ALU;
 
 architecture Behavioral of ALU is
-
-begin
-
-
+    signal CCR: std_logic_vector( 3 downto 0);
+    signal ALU_result: std_logic_vector(8 downto 0); -- 9 bits pour le carry
+begin 
+-- Z <= 1 si tout les bits sont à 0 (3)
+-- N si negatif (si le msb de oper à 1) (0)
+-- V si overflow (1)
+-- C si il y a eu une carry (si le 9ième bits est à un (carry car en dehors de operande)) (2)
+case Opcode_i is
+    when LOADconst | LOADaddr =>
+        ALU_result <= '0' & Oper1_i;
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if; 
+    when ANDconst | ANDaddr =>
+        ALU_result <= '0' & (Oper1_i and Oper2_i);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1'; 
+    when ORconst | ORaddr =>
+        ALU_result <= '0' & (Oper1_i or Oper2_i);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if; 
+    when XORconst | XORaddr =>
+        ALU_result <= Oper1_i xor Oper2_i);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if; 
+    when ROLaccu =>
+        ALU_result <= Oper1_i & CCR_i(2);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if; 
+    when RORaccu =>
+        ALU_result <= CCR_i(2) & Oper1_i;
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1'; 
+        end if;
+    when ADDconst =>
+        ALU_result <= std_logic_vector(('0' & unsigned(Oper1_i)) + ('0' & unsigned(Oper2_i)));
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+        if ALU_result = '111111111' then --a pas trouvé
+            CCR(1) <= '1';
+        end if;
+    when ADDaddr =>
+        ALU_result <= std_logic_vector(('0' & unsigned(Oper1_i)) + ('0' & unsigned(Oper2_i)));
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+        if ALU_result = '111111111' then --a pas trouvé
+            CCR(1) <= '1';
+        end if;
+    when ADCconst =>
+        ALU_result <= std_logic_vector(('0' & unsigned(Oper1_i)) + ('0' & unsigned(Oper2_i)) + CCR_i(2));
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+        if ALU_result = '111111111' then --a pas trouvé
+            CCR(1) <= '1';
+        end if;
+    when ADCaddr =>
+        ALU_result <= std_logic_vector(('0' & unsigned(Oper1_i)) + ('0' & unsigned(Oper2_i)) + CCR_i(2));
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+        if ALU_result = '111111111' then --a pas trouvé
+            CCR(1) <= '1';
+        end if;
+    when NEGaccu =>
+        ALU_result <= '0' std_logic_vector(not(unsigned(Oper1_i))+1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+    when NEGconst =>
+        ALU_result <= '0' std_logic_vector(not(unsigned(Oper1_i))+1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+    when NEGaddr =>
+        ALU_result <= '0' std_logic_vector(not(unsigned(Oper1_i))+1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+    when INCaccu =>
+        ALU_result <= std_logic_vector(unsigned('0' + Oper1_i)+1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+    when INCaddr =>
+        ALU_result <= std_logic_vector(unsigned('0' + Oper1_i)+1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+    when DECaccu =>
+        ALU_result <= std_logic_vector(unsigned('0' + Oper1_i)-1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+    when DECaddr =>
+        ALU_result <= std_logic_vector(unsigned('0' + Oper1_i)-1);
+        if ALU_result = (others => '0') then
+            CCR(3) <= '1';
+        end if;
+        if ALU_result(7) = '1' then
+            CCR(0) <= '1';
+        end if;
+        if ALU_result(8) = '1' then
+            CCR(2) <= '1';
+        end if;
+    when SETC =>
+        CR(2) <= '1';     
+    when CLRC =>
+        CCR(2) <= '0';
+    when TRFNC =>
+        CCR(2) <= CCR_i(0);
+    when others =>
+        CCR <= (others => '0');
+        ALU_result <= (others => '0');
+end case;
+CCR_alu_o <= CCR;
+Accu_o <= ALU_result(7 downto 0);
 end Behavioral;
